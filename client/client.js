761c695a1f1aecdd3fff0b90f5c34a07cec5cac0
СвятоСвят <qureqr@gmail.com>
2024-05-22 18:21:10 +0300

    написал базу для обработки и отправки сообщений

--- client/client.js
+++ client/client.js
@@ -1,2 +1,11 @@
-const { net } = require('shared/const.js');
+const { net } = require('../shared/const.js');
 
+const client = new net.Socket();
+client.connect(8000, 'localhost', () => {
+    console.log('Подключено к серверу');
+    client.write('Привет, сервер!');
+});
+
+client.on('data', (data) => {
+    console.log('Получено сообщение от сервера:', data.toString());
+});
--- server/core.js
+++ server/core.js
@@ -1,1 +1,12 @@
-const { net } = require('shared/const.js');
+const { net } = require('../shared/const.js');
+
+const server = net.createServer((socket) => {
+    socket.on('data', (data) => {
+        console.log('Получено сообщение от клиента:', data.toString());
+        socket.write('Сообщение получено и обработано');
+    });
+});
+
+server.listen(8000, () => {
+    console.log('Сервер запущен на порту 8000');
+});
