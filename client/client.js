f2c73722ffd0032e4542edb5f60ac50b54ce69ff
СвятоСвят <qureqr@gmail.com>
2024-05-22 18:42:07 +0300

    фикс бага, который не позволял написать больше одного сообщения

--- client/client.js
+++ client/client.js
@@ -5,8 +5,10 @@ const { net } = require('../shared/const.js');
 const client = new net.Socket();
 client.connect(8000, 'localhost', async () => {
     console.log('Подключено к серверу');
-    const message = await getInput('Введите сообщение: ');
-    client.write(message);
+    while (true) {
+        const message = await getInput('Введите сообщение: ');
+        client.write(message);
+    }
 });
 
 client.on('data', (data) => {
