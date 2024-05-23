f9d15f668d9c6b5a02127aa7b434b2db87be8fd8
СвятоСвят <qureqr@gmail.com>
2024-05-23 23:18:49 +0300

    переписан код чут чут, теперь от одного клиента остальным отсылаются месаге

--- client/client.js
+++ client/client.js
@@ -1,15 +1,32 @@
-const getInput = require('../shared/utils.js');
+// Клиент
+const readline = require('readline');
 const { net } = require('../shared/const.js');
+const { EventEmitter } = require('events');
 
 const client = new net.Socket();
-client.connect(8000, 'localhost', async () => {
+const eventEmitter = new EventEmitter();
+
+client.connect(8000, 'localhost', () => {
     console.log('Подключено к серверу');
-    while (true) {
-        const message = await getInput('Введите сообщение: ');
-        client.write(message);
-    }
+    eventEmitter.emit('connected');
 });
 
 client.on('data', (data) => {
-    console.log('Получено сообщение от сервера:', data.toString());
+    console.log('\nПолучено сообщение:', data.toString());
+    process.stdout.write('Введите сообщение: ');
+});
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+eventEmitter.on('connected', () => {
+    rl.setPrompt('Введите сообщение: ');
+    rl.prompt();
+
+    rl.on('line', (line) => {
+        client.write(line);
+        rl.prompt();
+    });
 });
